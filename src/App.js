17c256bec85d0ae347589ee58f4a2a73e302806e
unknown <kayn.pleiades@gmail.com>
2021-05-21 20:52:47 -0400

    render from json to the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
+import employees from "./employees.json";
 
-function App() {
-  return (
-    <div class="container">
-      <nav class="navbar navbar-light bg-light">
-        <span class="navbar-brand mb-0 h1">Employee Directory</span>
-      </nav>
-    </div>
-  );
+class App extends Component {
+  state = {
+    employees
+  };
+
+  render() {
+    return (
+      <div class="container">
+        <nav class="navbar navbar-light bg-light">
+          <span class="navbar-brand mb-0 h1">Employee Directory</span>
+        </nav>
+        <div class="row">
+          <div class="col">
+            {this.state.employees.map(employee => (
+              <p>
+                {employee.name}
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
