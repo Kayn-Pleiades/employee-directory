44ebb054b2291ab8a548a0b2353d69a56de80432
unknown <kayn.pleiades@gmail.com>
2021-05-22 15:41:59 -0400

    dropdown working

--- src/components/FilterButton/index.js
+++ src/components/FilterButton/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Collapse from 'react-bootstrap/Collapse'
+import GenderFilter from "../GenderFilter/index";
 
 
 function FilterButton() {
@@ -16,9 +17,7 @@ function FilterButton() {
             </Button>
             <Collapse in={open}>
                 <div id="example-collapse-text">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-                    terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-                    labore wes anderson cred nesciunt sapiente ea proident.
+                    <GenderFilter />
                 </div>
             </Collapse>
         </>
