961981f72556389947e0cdd3448aba601172cea4
unknown <kayn.pleiades@gmail.com>
2021-05-22 15:30:10 -0400

    filter dropdown now works

--- src/components/ContentController/index.js
+++ src/components/ContentController/index.js
@@ -1,15 +1,11 @@
 import React from "react";
+import FilterButton from "../FilterButton/index";
 
 function ContentController() {
     return (
         <div>
             <button type="button" class="btn btn-primary">Primary</button>
-            <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                Button with data-target
-            </button>
-            <div class="collapse" id="collapseExample">
-                <p>stuff goes here</p>
-            </div>
+            <FilterButton />
         </div>
     )
 }
